2e216c46fb15bea8e4bde948229d2de9947448a3
Abdessalam BENHARIRA <belmehelabdessalam.benharira@ynov.com>
2019-08-24 18:23:42 +0200

    feat: cors

--- functions/app/index.js
+++ functions/app/index.js
@@ -19,16 +19,16 @@ export default function expressApp(functionName) {
     routes = require("./routes"),
     app = express(),
     router = express.Router();
-  // CORS
-  //   app.use((req, res, next) => {
-  //     // res.header("Access-Control-Allow-Origin", "*");
-  //     // res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-  //     // res.header(
-  //     //   "Access-Control-Allow-Headers",
-  //     //   "Origin, X-Requested-With, Content-Type, Accept"
-  //     // );
-  //     next();
-  //   });
+  // CORS;
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
 
   app.use(logger("dev"));
   // security
