e2366d01f7de6b6228cc9f03e26730f34ff07d6d
Abdessalam BENHARIRA <belmehelabdessalam.benharira@ynov.com>
2019-10-01 07:55:15 +0200

    fix: ids controller

--- functions/app/routes/api/ids.js
+++ functions/app/routes/api/ids.js
@@ -1,5 +1,5 @@
 const router = require("express").Router(),
   IdsController = require("../../controllers/IdsController");
 
-router.route("/:count((\\d+){1,2})").get(IdsController.getMultipleViewIDs);
+router.route("/:count((\\d+){1,2})").get(IdsController.getIDs);
 module.exports = router;
